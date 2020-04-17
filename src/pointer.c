d702f223052803ce0f73cf36ec03b556de2a0731
georgii <immoondoom@gmail.com>
2020-04-17 11:25:02 +0300

    fix %p specificator

--- src/pointer.c
+++ src/pointer.c
@@ -1,19 +1,26 @@
 #include "../inc/ft_printf.h"
 
-char	*ft_ptoa(unsigned long int address) {
-	
-	unsigned long int	n;
-	int					i = 14;
-	char				*result;
-	char				*hex;
+static int		ft_get_nb_size_hex(unsigned long int num) {
+	int i = 0;
+	while (num) {
+		num /= 16;
+		i++;
+	}
+	return i;
+}
 
-	hex = "0123456789abcdef";
-	result = (char *)malloc(sizeof(char) * 15);
-	result[i--] = '\0';
-	n = address;
-	while (n) {
-		result[i--] = hex[n % 16];
-		n /= 16;
+char	*ft_ptoa(unsigned long int address) {
+	int					result_size = ft_get_nb_size_hex(address) + 2;
+	int					i;
+	char				*result;
+	char				*hex = "0123456789abcdef";;
+
+	result = (char *)malloc(sizeof(char) * result_size + 1);
+	result[result_size] = '\0';
+	i = result_size - 1;
+	while (address) {
+		result[i--] = hex[address % 16];
+		address /= 16;
 	}
 	result[1] = 'x';
 	result[0] = '0';
